6c10a18045600df8e107caf65d32c454e2564e44
Katerina Efim <katharina.efimov@web.de>
2020-11-09 14:25:29 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
-    if @question == 'I am going to work'
-      @answer = 'Great!'
-    elsif @question[-1] == '?'
-      @answer = 'Silly question, get dressed and go to work!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
+# .
